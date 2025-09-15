4286fc1f2710ab7e6b23e4c3e4ff2ead08cea151
diegoespinozareyna <diegoespinozareyna@gmail.com>
2025-09-15 01:23:45 -0500

    fix sendpago exitoso

--- handlers/handleReceptionPayProntopago.js
+++ handlers/handleReceptionPayProntopago.js
@@ -1,7 +1,33 @@
+import axios from "axios";
+
 export const handleReceptionPayProntopago = async (req, res) => {
     try {
         const body = req.body;
         console.log("body: ", body)
+
+        const optionsBody = JSON.stringify(body)
+
+        const response = await axios.post(
+            "https://apipozosanto.inmobackend.site/api/auth/receptionPayProntopaga",
+            optionsBody,
+            {
+                headers: {
+                    accept: "application/json",
+                    "content-type": "application/json",
+                },
+                validateStatus: () => true, // <- para capturar también errores 4xx/5xx
+            }
+        );
+
+        console.log("Respuesta cruda:", response.data);
+
+        return res.status(201).json({
+            message: "prespuesta pp correcta",
+            data: response.data,
+            dataCruda: response,
+            body: body,
+            status: 201,
+        });
 
     } catch (error) {
         console.error("Error al insertar url de pago:", error);
